e30a0883b3bc09387235f246fee49091a18238ba
Jacek Sieka <arnetheduck@gmail.com>
2017-04-19 00:04:08 +0800

    avoid pushd

--- make-llvm.sh
+++ make-llvm.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
-pushd ext
+cd ext
 
 [[ -f libLLVM-3.9.so ]] && exit 0
 
 [[ -f llvm-3.9.0.src.tar.xz ]] || wget http://llvm.org/releases/3.9.0/llvm-3.9.0.src.tar.xz || exit 1
 
-tar xvf llvm-3.9.0.src.tar.xz || exit 1
-pushd llvm-3.9.0.src
+tar xf llvm-3.9.0.src.tar.xz || exit 1
+cd llvm-3.9.0.src
 mkdir -p rel
-pushd rel
+cd rel
 cmake -DCMAKE_BUILD_TYPE=Release -DLLVM_BUILD_LLVM_DYLIB=1 -DLLVM_TARGETS_TO_BUILD=X86 -DLLVM_ENABLE_ASSERTIONS=1 ..
 
 make -j$(nproc) || exit 1
 
-popd
-popd
+cd ..
+cd ..
 
 ln -s llvm-3.9.0.src/rel/lib/libLLVM-3.9.so .
