507caf1ab8e58e02f82da97eb2f2609a038df2f4
Jacek Sieka <arnetheduck@gmail.com>
2020-12-29 23:00:23 +0100

    more dl links

--- dl-llvm.sh
+++ dl-llvm.sh
@@ -16,15 +16,16 @@ TGT="$4"
 
 LLVM_ROOT=llvm-$VER2.src
 
+[ -f clang+llvm-$VER2-x86_64-linux-gnu-ubuntu-20.04/bin/llvm-config ] || {
 
-[ -f clang+llvm-$VER2-x86_64-linux-gnu-ubuntu-14.04.tar.xz ] || {
+[ -f clang+llvm-$VER2-x86_64-linux-gnu-ubuntu-20.04.tar.xz ] || {
   wget https://github.com/llvm/llvm-project/releases/download/llvmorg-$VER2/clang+llvm-$VER2-x86_64-linux-gnu-ubuntu-20.04.tar.xz
 }
 
-[ -f clang+llvm-$VER2-x86_64-linux-gnu-ubuntu-18.04/bin/llvm-config ] || {
-  tar xvf clang+llvm-$VER2-x86_64-linux-gnu-ubuntu-18.04.tar.xz
+  tar xvf clang+llvm-$VER2-x86_64-linux-gnu-ubuntu-20.04.tar.xz
 }
 
 mkdir -p $LLVM_ROOT/
 rm -rf $LLVM_ROOT/$TGT
-ln -sr clang+llvm-$VER2-x86_64-linux-gnu-ubuntu-18.04 $LLVM_ROOT/$TGT
+ln -sr clang+llvm-$VER2-x86_64-linux-gnu-ubuntu-20.04 $LLVM_ROOT/$TGT
+
